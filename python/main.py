2a42a3d884058b14bc79f0fb17c948745906565a
derekatdwtech <derekcwilliams@protonmail.com>
2021-06-10 23:54:14 -0400

    fix: TRying to work out bugs

--- python/main.py
+++ python/main.py
@@ -19,7 +19,8 @@ headers={'Api-Key': API_KEY}
 #Initialize Logger
 logging.basicConfig(filename='pytempaast.log', filemode='a')
 
-if not PROBE_DIR or PROBE_NAME or API_KEY:
+
+if not PROBE_DIR or  not PROBE_NAME or not API_KEY:
     logging.error("Usage: 'python3 main.py [PROBE_DIRECTORY] [PROBE_NAME] [API_KEY]")
 
 #Initialize REST Module
@@ -27,20 +28,19 @@ rest =  Rest(API_URI)
 
 # Validate API_KEY. This will also return basic user Information
 logging.info("Validating API Key...")
-user_id = rest.Get("api/key/validate", headers)
-
+user_id = rest.Get("api/key/validate", headers)['userId']
+logging.debug("User ID is " + user_id)
 # Get or Create Probe Configuration
 logging.info("Attempting to get probe config from the API")
-
 probeConfig = rest.Get("api/probe/config/" + PROBE_NAME, headers)
-if probeConfig == "":
+if  probeConfig is None:
     logging.info("No configuration for this device was found. Creating with base configuration...")
     PROBE_ID=PROBE_DIR.split("/")
     BASE_CONFIG={"partitionKey": user_id, "rowKey": PROBE_ID[len(PROBE_ID) - 1], "nickname": PROBE_NAME, "readingIntervalInSeconds": 300, "tempThresholdInCelcius": 0, "user_id": user_id}
     probeConfig = rest.Post("api/probe/config", BASE_CONFIG, headers)
     
 probe = Probe(PROBE_NAME, PROBE_DIR, 'w1_slave')
-
+print(probeConfig)
 while True:
     sleepTime = probeConfig['readingIntervalInSeconds']
     logging.info("Probe interval read as " + str(sleepTime))
