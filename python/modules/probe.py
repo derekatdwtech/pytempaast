9f76e5d13df2af2fb35146e731ba4d5139cb6ed2
derekatdwtech <derekcwilliams@protonmail.com>
2021-06-17 21:57:00 -0400

    fix: Check if is list

--- python/modules/probe.py
+++ python/modules/probe.py
@@ -67,7 +67,7 @@ class Probe:
             logger.warn("No probe configuration was found for probe " + PROBE_ID + ". Creating new configuration with config base...")
             conf = requests.post(self.config.GetApiUri() + "api/probe/config", headers=self.config.GetApiHeaders(), data=json.dumps(BASE_CONFIG))
             if conf.status_code == 200:
-                return json.loads(conf.content.decode("UTF-8"))
+                return [json.loads(conf.content.decode("UTF-8"))]
             else:
                 logger.error("Unable to create new probe configuration.")
                 return json.loads({})
