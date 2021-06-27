31e814b07fab216c23a26899437e55fbefc9ab14
derekatdwtech <derekcwilliams@protonmail.com>
2021-06-27 11:24:59 -0400

    feat: Adding fall back configurations

--- python/main.py
+++ python/main.py
@@ -39,9 +39,8 @@ while True:
     # Check for backed messages
     message.CheckForBackUpMessages(config.GetTemperatureQueue())
     # Get Probe Configuration
-    # TODO: If cannot get probe config, fallback to last config reading
     pc = probe.GetProbeConfig(user_id)[0]
-    sleepTime = pc['readingIntervalInSeconds']
+    sleepTime = pc.probeConfig['readingIntervalInSeconds']
     
     logger.info("Probe read interval read as " + str(sleepTime))
     result = probe.readTemp(user_id)
